b1d39ea721b65f0e9f14b9c808b62ac76735d9f5
Naufal Ulinnuha <naufalulinnuha40@gmail.com>
2024-09-06 19:48:54 +0700

    Add sweetalert

--- resources/views/dashboard/production.blade.php
+++ resources/views/dashboard/production.blade.php
@@ -18,15 +18,6 @@
             </div>
         </div>
         <div class="card-body">
-            @if(session('success'))
-                <div class="alert alert-success">
-                    {{ session('success') }}
-                </div>
-            @elseif(session('error'))
-                <div class="alert alert-danger">
-                    {{ session('error') }}
-                </div>
-            @endif
             <div class="table-responsive">
                 <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
                     <thead>
--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -6,6 +6,21 @@
 </head>
 
 <body id="page-top">
+
+  <script>
+    @if(session()->has('success'))
+      Swal.fire({title:'Berhasil', text:'{{session('success')}}', icon:'success'})
+    @endif
+    @if(session()->has('error'))
+      Swal.fire({title:'Error!', text:'{{session('error')}}', icon:'error'})
+    @endif
+    @if(session()->has('info'))
+      Swal.fire({title:'Info', text:'{{session('info')}}', icon:'info'})
+    @endif
+    @if($errors->any())
+      Swal.fire({title:'Error!', html:'{!! implode('', $errors->all(':message<br>')) !!}', icon:'error'})
+    @endif
+  </script>
   
   <!-- Page Wrapper -->
   <div id="wrapper">
